b2f377c11283c13c93b2b1394d3ec5f258633c3a
Nikita Demianok <nikita@demianok.me>
2021-06-11 19:06:59 +0300

    draft

--- init_setup.sh
+++ init_setup.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
 
-echo alias ll=\'ls -half\'
-echo alias inst=\'sudo apt update ; sudo apt install\' >> /etc/bash.bashrc
-echo alias instt=\'sudo apt install\' >> /etc/bash.bashrc
-echo alias mkt=\'cd $(mktemp -d)\' >> /etc/bash.bashrc
+echo "" | sudo tee -a /etc/bash.bashrc
+
+echo alias ll=\'ls -half\' | sudo tee -a /etc/bash.bashrc
+echo alias inst=\'sudo apt update \; sudo apt install\' | sudo tee -a /etc/bash.bashrc
+echo alias instt=\'sudo apt install\' | sudo tee -a /etc/bash.bashrc
+echo alias mkt=\'cd \$\(mktemp -d\)\' | sudo tee -a /etc/bash.bashrc
 
 sudo apt update
 
 sudo apt install -y ubuntu-dbgsym-keyring
 sudo apt install -y xclip
 
-echo alias xc=\'xargs echo -n | xclip -selection clipboard\' >> /etc/bash.bashrc
-echo alias xcp=\'pwd | xc\' >> /etc/bash.bashrc
+echo alias xc=\'xargs echo -n \| xclip -selection clipboard\' | sudo tee -a /etc/bash.bashrc
+echo alias xcp=\'pwd \| xc\' | sudo tee -a /etc/bash.bashrc
 
+
+sudo apt install -y vim
 sudo apt install -y gparted
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -20,6 +25,4 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt update
 sudo apt install -y sublime-text
 
-sudo apt-add-repository ppa:tista/adapta
-sudo apt update
 sudo apt install -y adapta-gtk-theme
---
+++ init_user.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+dconf write /org/gnome/libgnomekbd/keyboard/options "['grp\tgrp:alt_shift_toggle', 'grp\tgrp:win_space_toggle']"
