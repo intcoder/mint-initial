6d2b27d786e58fbd030140daaa3c5cb489a0a7a3
Nikita Demianok <nikita@demianok.me>
2021-06-12 23:35:39 +0300

    init_setup.sh: tree, pwgen

--- init_setup.sh
+++ init_setup.sh
@@ -19,6 +19,8 @@ echo alias xcp=\'pwd \| xc\' | sudo tee -a /etc/bash.bashrc
 sudo apt install -y vim
 sudo apt install -y gparted
 sudo apt install -y meld
+sudo apt install -y tree
+sudo apt install -y pwgen
 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt install -y apt-transport-https
